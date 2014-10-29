87a1b2eaab70ac20b1db8ade384cc52fa3e33001
Eduardo Feo <eduardo@idsia.ch>
2014-10-29 15:21:31 +0100

    Added lcm tests

---
+++ src/lcmtest/fakelistener.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#include <lcm/lcm-cpp.hpp>
+#include "lcmtypes/lcmmsgs.hpp"
+
+class Handler 
+{
+    public:
+        ~Handler() {}
+
+        void handleMessage(const lcm::ReceiveBuffer* rbuf,
+                const std::string& chan, 
+                const poselcm::pose_list_t* msg)
+        {
+            int i;
+            printf("Received message on channel \"%s\":\n", chan.c_str());
+            printf("  timestamp   = %lld\n", (long long)msg->timestamp);
+            printf("  poses:");
+            for(i = 0; i < msg->n; i++)
+	    {
+	      const poselcm::pose_t &pose = msg->poses[i];
+	      printf("  robotid = %d\n", pose.robotid);
+	      printf("  position    = (%d, %d, %d)\n",
+		     pose.position[0], pose.position[1], pose.position[2]);
+	      printf("  orientation = (%d, %d, %d, %d)\n",
+		     pose.orientation[0], pose.orientation[1], 
+		     pose.orientation[2], pose.orientation[3]);
+
+	    }
+            printf("\n");
+        }
+};
+
+int main(int argc, char** argv)
+{
+    lcm::LCM lcm;
+
+    if(!lcm.good())
+        return 1;
+
+    Handler handlerObject;
+    lcm.subscribe("EXAMPLE", &Handler::handleMessage, &handlerObject);
+
+    while(0 == lcm.handle());
+
+    return 0;
+}
--- src/lcmtest/fakepublisher.cc
+++ src/lcmtest/fakepublisher.cc
@@ -1,16 +1,6 @@
-// file: send_message.cpp
-//
-// LCM example program.
-//
-// compile with:
-//  $ g++ -o send_message send_message.cpp -llcm
-//
-// On a system with pkg-config, you can also use:
-//  $ g++ -o send_message send_message.cpp `pkg-config --cflags --libs lcm`
-
 #include <lcm/lcm-cpp.hpp>
 
-#include "exlcm/example_t.hpp"
+#include "lcmtypes/lcmmsgs.hpp"
 
 int main(int argc, char ** argv)
 {
@@ -18,27 +8,27 @@ int main(int argc, char ** argv)
     if(!lcm.good())
         return 1;
 
-    exlcm::example_t my_data;
-    my_data.timestamp = 0;
+    poselcm::pose_list_t mymsg;
+    mymsg.n = 5;
+    mymsg.poses.resize(mymsg.n);
+    for( int i=0; i < mymsg.n; i++)
+    {
+      poselcm::pose_t my_data;
+      my_data.robotid = i;
+      my_data.position[0] = 1;
+      my_data.position[1] = 2;
+      my_data.position[2] = 3;
 
-    my_data.position[0] = 1;
-    my_data.position[1] = 2;
-    my_data.position[2] = 3;
+      my_data.orientation[0] = 1;
+      my_data.orientation[1] = 0;
+      my_data.orientation[2] = 0;
+      my_data.orientation[3] = 0;
+      mymsg.poses[i] = my_data;
+    }
+    printf("Filled message with 5 poses\n");
+    mymsg.timestamp = time( NULL );
 
-    my_data.orientation[0] = 1;
-    my_data.orientation[1] = 0;
-    my_data.orientation[2] = 0;
-    my_data.orientation[3] = 0;
-
-    my_data.num_ranges = 15;
-    my_data.ranges.resize(my_data.num_ranges);
-    for(int i = 0; i < my_data.num_ranges; i++)
-        my_data.ranges[i] = i;
-
-    my_data.name = "example string";
-    my_data.enabled = true;
-
-    lcm.publish("EXAMPLE", &my_data);
+    lcm.publish("EXAMPLE", &mymsg);
 
     return 0;
 }
