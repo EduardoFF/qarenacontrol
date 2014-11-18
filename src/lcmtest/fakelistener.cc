eb18562d822fbf13dd1f82b2a3595b3e254b4c8f
robertomagan <rmagan@ugr.es>
2014-11-18 12:55:45 +0100

    Deleted!

--- src/lcmtest/fakelistener.cc
+++ src/lcmtest/fakelistener.cc
@@ -3,47 +3,46 @@
 #include <lcm/lcm-cpp.hpp>
 #include "lcmtypes/lcmmsgs.hpp"
 
-class Handler 
-{
-    public:
-        ~Handler() {}
+class Handler {
+public:
+	~Handler() {
+	}
 
-        void handleMessage(const lcm::ReceiveBuffer* rbuf,
-                const std::string& chan, 
-                const poselcm::pose_list_t* msg)
-        {
-            int i;
-            printf("Received message on channel \"%s\":\n", chan.c_str());
-            printf("  timestamp   = %lld\n", (long long)msg->timestamp);
-            printf("  poses:\n");
-            for(i = 0; i < msg->n; i++)
-				{
-				  const poselcm::pose_t &pose = msg->poses[i];
-				  printf("  robotid = %d\n", pose.robotid);
-				  printf("  position    = (%d, %d, %d)\n",
-					 pose.position[0], pose.position[1], pose.position[2]);
-				  printf("  orientation = (%d, %d, %d, %d)\n",
-					 pose.orientation[0], pose.orientation[1],
-					 pose.orientation[2], pose.orientation[3]);
-				  printf(" velocity = %d\n", pose.velocity);
+	void handleMessage(const lcm::ReceiveBuffer* rbuf, const std::string& chan, const poselcm::pose_list_t* msg) {
+		int i;
+		printf("Received message on channel \"%s\":\n", chan.c_str());
+		printf("  timestamp   = %lld\n", (long long) msg->timestamp);
+		printf("  poses:\n");
+		for (i = 0; i < msg->n; i++) {
+			const poselcm::pose_t &pose = msg->poses[i];
+			printf("  robotid = %d\n", pose.robotid);
+			printf("  position    = (%d, %d, %d)\n", pose.position[0], pose.position[1], pose.position[2]);
+			printf("  orientation = (%d, %d, %d, %d)\n", pose.orientation[0], pose.orientation[1], pose.orientation[2], pose.orientation[3]);
+			printf(" velocity = %d\n", pose.velocity);
 
-				}
-					printf("\n");
-				}
+		}
+		printf("\n");
+	}
 };
 
-int main(int argc, char** argv)
-{
-    lcm::LCM lcm ("udpm://239.255.76.67:7667?ttl=1");
+int main(int argc, char** argv) {
+	lcm::LCM lcm("udpm://239.255.76.67:7667?ttl=1");
 	//lcm::LCM lcm;
+	if (argc < 2) {
+		printf("\nUse : ./fakelistener <LCM channel>\n\n");
+	} else {
 
-    if(!lcm.good())
-        return 1;
+		if (!lcm.good()) {
+			printf("Error initializing LCM engine ...");
+			return 1;
+		}
 
-    Handler handlerObject;
-    lcm.subscribe("EXAMPLE", &Handler::handleMessage, &handlerObject);
+		Handler handlerObject;
+		lcm.subscribe(argv[1], &Handler::handleMessage, &handlerObject);
 
-    while(0 == lcm.handle());
+		while (0 == lcm.handle())
+			;
+	}
 
-    return 0;
+	return 0;
 }
